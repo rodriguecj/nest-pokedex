162e8f0df009c511cbaa43199999ee191c79f6c4
jrodriguez <jose.rodriguez@bice.cl>
2023-07-16 19:35:45 -0400

    Feature X

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World from A!';
+    const feature1 = this.configService.get('FEATURE');
+    return `Hello World from ${feature1}! `;
   }
 }
