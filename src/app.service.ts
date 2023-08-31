0731e00ae5988f0f43d3d20445cf9ba0735bb989
jrodriguez <jose.rodriguez@bice.cl>
2023-08-30 21:19:42 -0400

    Keisel - Test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from A with AWS!!';
+    return 'Hello World from A with AWS con Keisel!!';
   }
 }
